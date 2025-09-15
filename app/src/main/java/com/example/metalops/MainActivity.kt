0dc23d9714e1ca7d4f45d5eab84b5654dfa9dc6e
AlcocerAA <alexanderalcocer21092728@gmail.com>
2025-09-15 18:52:52 -0500

    Agregada HomeScreen, BottomBar y pantallas de Clientes y OT

--- app/src/main/java/com/example/metalops/MainActivity.kt
+++ app/src/main/java/com/example/metalops/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.metalops
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+<<<<<<< HEAD
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
@@ -10,16 +12,21 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.metalops.ui.BottomBar
+=======
+import androidx.annotation.RequiresApi
+>>>>>>> 6d82b9db836fbcc6314e86caee63b05c24b885fb
 import com.example.metalops.ui.HomeScreen
 import com.example.metalops.ui.ClientesScreen
 import com.example.metalops.ui.OTScreen
 import com.example.metalops.ui.theme.MetalOpsTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MetalOpsTheme {
+<<<<<<< HEAD
                 val navController = rememberNavController()
 
                 Scaffold(
@@ -35,6 +42,9 @@ class MainActivity : ComponentActivity() {
                         composable("ots") { OTScreen() }
                     }
                 }
+=======
+                HomeScreen() // Solo la pantalla de inicio
+>>>>>>> 6d82b9db836fbcc6314e86caee63b05c24b885fb
             }
         }
     }
